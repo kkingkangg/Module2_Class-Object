f27a94fcc52f5533f7f147ad93b18300d5eaa5b6
kkingkangg <phungnghia152@gmail.com>
2020-09-14 16:35:35 +0700

    hinh chu nhat

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
 public class Rectangle {
-    public static void main(String[] args) {
+    double width, height;
 
+//    public Rectangle () {
+//
+//    }
+
+    public Rectangle(double width, double height){
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.width * this.height;
+    }
+
+    public double getPerimeter() {
+        return (this.width+this.height)*2;
+    }
+
+    public String display() {
+        return "Rectangle{" + "width=" + width + ", height=" + height + "}";
+    }
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        double r = input.nextDouble();
+        double d = input.nextDouble();
+        Rectangle hcn = new Rectangle(r,d);
+        System.out.println("Your Rectangle \n"+ hcn.display());
+        System.out.println("Perimeter of the Rectangle: "+ hcn.getPerimeter());
+        System.out.println("Area of the Rectangle: "+ hcn.getArea());
     }
 }
